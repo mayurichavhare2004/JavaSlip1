cc388c16277f7570ca373881cf9c540ae6261aaf
Chavhare Mayuri Sudam <144438613+mayurichavhare2004@users.noreply.github.com>
2024-10-15 03:10:32 -0700

    17

--- Mynumber.java
+++ Mynumber.java
@@ -1,76 +1,72 @@
-package Slip2;
+package Slip16;
 
-
-
-
- 
-
-	import java.io.*;
-	class Mynumber
+import java.io.*;
+class Mynumber
+{
+	 int a;
+	Mynumber()
 	{
-		 int a;
-		Mynumber()
-		{
-			a=0;
-		}
-		Mynumber(int a)
-		{
-			this.a=a;
-		}
-		boolean ispositive(int a)
-		{
-			if(a>0)
-			return true;
-			else
-			return false;
-		}
-		boolean isnegative(int a)
-		{
-			if(a<0)
-			return true;
-			else
-			return false;	
-		}
-		boolean iseven(int a)
-		{
-			if(a%2==0)
-			return true;
-			else
-			return false;
-		}
-		boolean isodd(int a)
-		{
-			if(a%2!=0)
-			return true;
-			else
-			return false;
-		}
-		boolean iszero(int a)
-		{
-			if(a==0)
-			return true;
-			else
-			return false;
-		}
+		a=0;
 	}
-
-	class Mynumber1
+	Mynumber(int a)
 	{
-		public static void main(String args[]) throws IOException
-		{
-			int a=Integer.parseInt(args[0]);
-			Mynumber m=new Mynumber();
-			if(m.isodd(a))
-			System.out.println("Odd");
-			if(m.iseven(a))
-			System.out.println("Even");
-			if(m.ispositive(a))
-			System.out.println("Positive");
-			if(m.isnegative(a))
-			System.out.println("Negative");
-			if(m.iszero(a))
-			System.out.println("Zero");
-		}
+		this.a=a;
 	}
+	boolean ispositive(int a)
+	{
+		if(a>0)
+		return true;
+		else
+		return false;
+	}
+	boolean isnegative(int a)
+	{
+		if(a<0)
+		return true;
+		else
+		return false;	
+	}
+	boolean iseven(int a)
+	{
+		if(a%2==0)
+		return true;
+		else
+		return false;
+	}
+	boolean isodd(int a)
+	{
+		if(a%2!=0)
+		return true;
+		else
+		return false;
+	}
+	boolean iszero(int a)
+	{
+		if(a==0)
+		return true;
+		else
+		return false;
+	}
+}
+
+class Mynumber1
+{
+	public static void main(String args[]) throws IOException
+	{
+		int a=Integer.parseInt(args[0]);
+		Mynumber m=new Mynumber();
+		if(m.isodd(a))
+		System.out.println("Odd");
+		if(m.iseven(a))
+		System.out.println("Even");
+		if(m.ispositive(a))
+		System.out.println("Positive");
+		if(m.isnegative(a))
+		System.out.println("Negative");
+		if(m.iszero(a))
+		System.out.println("Zero");
+	}
+}
+
 
 
