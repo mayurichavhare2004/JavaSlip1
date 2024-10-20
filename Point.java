596df82c7a452a1168cc2be8515b8ac8a9f254e7
Chavhare Mayuri Sudam <144438613+mayurichavhare2004@users.noreply.github.com>
2024-10-20 01:25:05 -0700

    slip my 9
    slip my 9

--- Point.java
+++ Point.java
@@ -1,36 +1,106 @@
-package Slip5;
+package array;
+class Point {
 
+protected int x, y;
 
-	class Point { 
-	    int x; 
-	    int y; 
-	    public Point() { 
-	        x = 0; 
-	        y = 0;} 
-	    public Point(int x, int y) { 
-	        this.x = x; 
-	        this.y = y;} 
-	    public void display() { 
-	        System.out.println("Coordinates: (" + x + ", " + y + ")");}} 
-	class ColorPoint extends Point { 
-	    String color; 
-	    public ColorPoint(int x, int y, String color) { 
-	        super(x, y);  
-	        this.color = color;} 
-	    @Override 
-	    public void display() { 
-	        super.display();  
-	        System.out.println("Color: " + color);}} 
-	public class Main { 
-	    public static void main(String[] args) { 
-	        Point point1 = new Point(); 
-	        System.out.println("Point 1:"); 
-	        point1.display(); 
-	        Point point2 = new Point(3, 4); 
-	        System.out.println("\nPoint 2:"); 
-	        point2.display(); 
-	        ColorPoint colorPoint = new ColorPoint(5, 6, "Red"); 
-	        System.out.println("\nColor Point:"); 
-	        colorPoint.display();}} 
+// Default constructor
 
+public Point() {
+
+x = 0;
+
+y = 0;
+
+}
+
+// Parameterized constructor
+
+public Point(int x, int y) {
+
+this.x = x;
+
+this.y = y;
+
+}
+
+// Display method
+
+public void display() {
+
+System.out.println("Point: (" + x + ", " + y + ")");
+
+}
+
+}
+
+class ColorPoint extends Point {
+
+private String color;
+
+// Parameterized constructor
+
+public ColorPoint(int x, int y, String color) {
+
+super(x, y);
+
+this.color = color;
+
+}
+
+// Display method
+
+@Override
+
+public void display() {
+
+System.out.println("ColorPoint: (" + x + ", " + y + "), Color: " + color);
+
+}
+
+}
+
+class Point3D extends Point {
+
+private int z;
+
+// Parameterized constructor
+
+public Point3D(int x, int y, int z) {
+
+super(x, y);
+
+this.z = z;
+
+}
+
+// Display method
+
+@Override
+
+public void display() {
+
+System.out.println("Point3D: (" + x + ", " + y + ", " + z + ")");
+
+}
+
+}
+public class Main2 {
+
+public static void main(String[] args) {
+
+Point point = new Point(2, 3);
+
+ColorPoint colorPoint = new ColorPoint(4, 5, "Red");
+
+Point3D point3D = new Point3D(1, 2, 3);
+
+point.display();
+
+colorPoint.display();
+
+point3D.display();
+
+}
+
+}
 
